918da13d34bef005cb8160befd22e73db26723ad
giordano <giordano.demarzo@gmail.com>
2025-05-25 21:49:24 +0200

    added info and team

--- assets/js/components/OptimizedPollutionAbatementDashboard.jsx
+++ assets/js/components/OptimizedPollutionAbatementDashboard.jsx
@@ -623,9 +623,10 @@ const OptimizedPollutionAbatementDashboard = () => {
       {/* Bottom contributor note - positioned at bottom of left panel */}
       <div className="absolute bottom-0 left-0 right-1/3 bg-gray-50 bg-opacity-95 border-t border-gray-200 p-2 text-center">
         <p className="text-xs text-gray-500">
-          <strong>Contributors:</strong> Frontend: G. De Marzo (Konstanz) • 
-          Data & ML: S. Aroyehun (Konstanz), G. De Marzo (Konstanz), E. Fenoaltea, F. Santoro, A. Tacchella (CREF)
-        </p>
+	  <strong>Contributors:</strong> Frontend: G. De Marzo<sup>1</sup> • 
+	  Data & ML: S. Aroyehun<sup>1</sup>, G. De Marzo<sup>1</sup>, E. Fenoaltea<sup>2</sup>, F. Santoro<sup>2</sup>, A. Tacchella<sup>2</sup> &nbsp; 
+	  <span className="ml-2"><sup>1</sup> Konstanz University, <sup>2</sup> CREF</span>
+	</p>
       </div>
       
       {/* Updated Pollutant Info Box with new SDG data */}
